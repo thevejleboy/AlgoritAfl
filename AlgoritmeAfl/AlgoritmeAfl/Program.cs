5d624620145189f0be80b6559b752b6175e7ff53
Arnar Palmi Asmundsson <arna0060@student.eadania.dk>
2016-10-03 12:40:52 +0200

    huehue

--- AlgoritmeAfl/AlgoritmeAfl/Program.cs
+++ AlgoritmeAfl/AlgoritmeAfl/Program.cs
@@ -16,7 +16,7 @@ namespace AlgoritmeAfl
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); //doakodsaodkaokdosa
         }
     }
 }
