0d38f151f474663bdd1121f4f169d68a2e82b13f
Arnar Palmi Asmundsson <arna0060@student.eadania.dk>
2016-10-07 00:30:25 +0200

    Scene sat op

--- AlgoritmeAfl/AlgoritmeAfl/Form1.cs
+++ AlgoritmeAfl/AlgoritmeAfl/Form1.cs
@@ -13,7 +13,6 @@ namespace AlgoritmeAfl
     public partial class Form1 : Form
     {
         private GridManager visualManager;
-        private WorldSetup worldSetup;
 
         public Form1()
         {
@@ -22,24 +21,41 @@ namespace AlgoritmeAfl
             ClientSize = new Size(800,800);
 
             visualManager = new GridManager(CreateGraphics(),this.DisplayRectangle);
-            
+           
+
         }
 
 
         private void Loop_Tick(object sender, EventArgs e)
         {
             visualManager.Render();
+            
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
             WorldSetup worldSetup = new WorldSetup();
-            foreach (Wall wall in worldSetup.CreateScene())
+            foreach (Wall wall in worldSetup.CreateWalls())
             {
                 visualManager.CellSpot(wall.Position, "WALL");
             }
-            
+            foreach (Tower tower in worldSetup.CreateTowers())
+            {
+                visualManager.CellSpot(tower.Position, "TOWER");
+            }
+            foreach (Key key in worldSetup.CreateKeys())
+            {
+                visualManager.CellSpot(key.Position, "KEY");
+            }
+            foreach (Tree tree in worldSetup.CreateTrees())
+            {
+                visualManager.CellSpot(tree.Position, "TREE");
+            }
+            visualManager.CellSpot(new Point(150, 600), "WIZARD");
+            visualManager.CellSpot(new Point(100, 600), "PORTAL");
+
         }
     }
 }
--- AlgoritmeAfl/AlgoritmeAfl/Tree.cs
+++ AlgoritmeAfl/AlgoritmeAfl/Tree.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace AlgoritmeAfl
 {
-    class Wall
+    class Tree
     {
-
         public Point Position { get; set; }
-
-        public Wall(Point position) 
+        public Tree(Point position)
         {
 
             this.Position = position;
-            
         }
     }
 }
